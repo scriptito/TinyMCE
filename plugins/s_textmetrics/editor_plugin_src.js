793a0d93e6584119ee319d97408feea52fe5126c
Scott Stafford <scott.stafford@gmail.com>
2012-07-03 15:49:38 -0400

    Fix innocuous javascript error if frame is closed.

--- plugins/s_textmetrics/editor_plugin_src.js
+++ plugins/s_textmetrics/editor_plugin_src.js
@@ -1,137 +1,140 @@
 /**
- */
+*/
 
-(function() {
+(function () {
 
-  tinymce.create('scriptito.plugins.TextMetrics', {
-    
-    block : 0,
-    tid: null,
-    ccid : null,
-    wcid : null,
-    scid : null,
-    pcid : null,
-    lcid : null,
+    tinymce.create('scriptito.plugins.TextMetrics', {
 
-    init : function(ed, url) {
-    
-      var t = this;
-      
-      t.editor = ed;
-      
-      t.tid = ed.id + "-metrics";
-      t.ccid = ed.id + '-character-count';
-      t.wcid = ed.id + '-word-count';
-      t.scid = ed.id + '-sentence-count';
-      t.pcid = ed.id + '-paragraph-count';
-      t.lcid = ed.id + '-line-count';
-  
-      t.state = ed.getParam('s_textmetrics_on');
-      
-      ed.addCommand("s_toggleMetrics", t._toggleMetrics, t);
-      
-      ed.addButton('toggle_metrics', { title:'s_textmetrics.button_title', cmd:'s_toggleMetrics' });
+        block: 0,
+        tid: null,
+        ccid: null,
+        wcid: null,
+        scid: null,
+        pcid: null,
+        lcid: null,
 
-      ed.onPostRender.add(function(ed, cm) {
-        var row, id;
-        id = ed.getParam('textmetrics_target_id');
-        if (!id) {
-          row = tinymce.DOM.get(ed.id + '_path_row');
-          if (row) {
-            tinymce.DOM.add(
-              row.parentNode, 
-              'div', 
-              { 'id':t.tid, 'class':'metrics', 'style':'display:' + (t.state ? "block;" : "none;") }, 
-              ed.getParam('s_textmetrics_template', function(ed, t) {
-                return '<span>' + ed.getLang('s_textmetrics.characters', 'Characters: ') + '<span id="' + t.ccid + '">0</span></span>' +
+        init: function (ed, url) {
+
+            var t = this;
+
+            t.editor = ed;
+
+            t.tid = ed.id + "-metrics";
+            t.ccid = ed.id + '-character-count';
+            t.wcid = ed.id + '-word-count';
+            t.scid = ed.id + '-sentence-count';
+            t.pcid = ed.id + '-paragraph-count';
+            t.lcid = ed.id + '-line-count';
+
+            t.state = ed.getParam('s_textmetrics_on');
+
+            ed.addCommand("s_toggleMetrics", t._toggleMetrics, t);
+
+            ed.addButton('toggle_metrics', { title: 's_textmetrics.button_title', cmd: 's_toggleMetrics' });
+
+            ed.onPostRender.add(function (ed, cm) {
+                var row, id;
+                id = ed.getParam('textmetrics_target_id');
+                if (!id) {
+                    row = tinymce.DOM.get(ed.id + '_path_row');
+                    if (row) {
+                        tinymce.DOM.add(
+              row.parentNode,
+              'div',
+              { 'id': t.tid, 'class': 'metrics', 'style': 'display:' + (t.state ? "block;" : "none;") },
+              ed.getParam('s_textmetrics_template', function (ed, t) {
+                  return '<span>' + ed.getLang('s_textmetrics.characters', 'Characters: ') + '<span id="' + t.ccid + '">0</span></span>' +
                   '<span>' + ed.getLang('s_textmetrics.words', 'Words: ') + '<span id="' + t.wcid + '">0</span></span>' +
                   '<span>' + ed.getLang('s_textmetrics.sentences', 'Sentences: ') + '<span id="' + t.scid + '">0</span></span>' +
                   '<span>' + ed.getLang('s_textmetrics.paragraphs', 'Paragraphs: ') + '<span id="' + t.pcid + '">0</span></span>' +
                   '<span>' + ed.getLang('s_textmetrics.lines', 'Lines: ') + '<span id="' + t.lcid + '">0</span></span>';
               })(ed, t)
             );
-          }
-          ed.controlManager.setActive('toggle_metrics', t.state);
-        } 
-        else {
-          tinymce.DOM.add(id, 'span', {}, '<span id="' + t.wcid + '">0</span>');
-        }
-      });
+                    }
+                    ed.controlManager.setActive('toggle_metrics', t.state);
+                }
+                else {
+                    tinymce.DOM.add(id, 'span', {}, '<span id="' + t.wcid + '">0</span>');
+                }
+            });
 
-      ed.onSetContent.add(function(ed) {
-        t._count(ed);
-      });
+            ed.onSetContent.add(function (ed) {
+                t._count(ed);
+            });
 
-      ed.onLoadContent.add(function(ed) {
-        t._count(ed);
-      });
+            ed.onLoadContent.add(function (ed) {
+                t._count(ed);
+            });
 
-      ed.onUndo.add(function(ed) {
-        t._count(ed);
-      });
-      
-      ed.onPaste.add(function(ed) {
-        t._count(ed);
-      });
+            ed.onUndo.add(function (ed) {
+                t._count(ed);
+            });
 
-      ed.onKeyUp.add(function(ed, e) {
-        if (sTextUtils.isWhitespaceKeyCode(e.keyCode) || sTextUtils.isPunctuationKeyCode(e.keyCode)
+            ed.onPaste.add(function (ed) {
+                t._count(ed);
+            });
+
+            ed.onKeyUp.add(function (ed, e) {
+                if (sTextUtils.isWhitespaceKeyCode(e.keyCode) || sTextUtils.isPunctuationKeyCode(e.keyCode)
           || 8 == e.keyCode // Backspace
           || 46 == e.keyCode) { // Delete
-          t._count(ed);
+                    t._count(ed);
+                }
+            });
+
+        },
+
+        _count: function (ed) {
+            var t = this, tc = 0;
+
+            if (!t.state || t.block)
+                return;
+
+            t.block = 1;
+
+            setTimeout(function () {
+                var tx = null;
+                try {
+                    tx = ed.getContent({ format: 'raw' });
+                } catch (e) { }
+                if (tx) {
+                    var d = tinymce.DOM;
+                    var m = sTextUtils.getMetricsForHtml(tx, ed.getParam('s_textmetrics_charsperline', 80));
+                    var updater = ed.getParam('s_textmetrics_updater', function (t, d, m) {
+                        d.setHTML(t.ccid, m.characterCount);
+                        d.setHTML(t.wcid, m.wordCount);
+                        d.setHTML(t.scid, m.sentenceCount);
+                        d.setHTML(t.pcid, m.paragraphCount);
+                        d.setHTML(t.lcid, m.lineCount);
+                    });
+                    updater(t, d, m);
+                }
+                t.block = 0;
+            }, 1);
+        },
+
+        _toggleMetrics: function () {
+            var t = this, ed = t.editor;
+            t.state = !t.state;
+            ed.controlManager.setActive('toggle_metrics', t.state);
+            tinymce.DOM.setStyle(t.tid, "display", t.state ? "block" : "none");
+            if (t.state) {
+                t._count(ed);
+            }
+        },
+
+        getInfo: function () {
+            return {
+                longname: 'Text Metrics Plugin',
+                author: 'Scriptito, LLC',
+                authorurl: 'http://www.scriptito.com',
+                infourl: 'http://www.scriptito.com',
+                version: "1.2.2"
+            };
         }
-      });
 
-    },
+    });
 
-    _count : function(ed) {
-      var t = this, tc = 0;
-  
-      if (!t.state || t.block)
-        return;
-  
-      t.block = 1;
-  
-      setTimeout(function() {
-        var tx = ed.getContent({format : 'raw'});
-        if (tx) {
-          var d = tinymce.DOM;
-          var m = sTextUtils.getMetricsForHtml(tx, ed.getParam('s_textmetrics_charsperline', 80));
-          var updater = ed.getParam('s_textmetrics_updater', function(t,d,m) {
-            d.setHTML(t.ccid, m.characterCount);
-            d.setHTML(t.wcid, m.wordCount);
-            d.setHTML(t.scid, m.sentenceCount);
-            d.setHTML(t.pcid, m.paragraphCount);
-            d.setHTML(t.lcid, m.lineCount);
-          });
-          updater(t,d,m);
-        }
-        t.block = 0;
-      }, 1);
-    },
-
-    _toggleMetrics:function() {
-      var t = this, ed = t.editor;
-      t.state = !t.state;
-      ed.controlManager.setActive('toggle_metrics', t.state);
-      tinymce.DOM.setStyle(t.tid, "display", t.state ? "block" : "none");
-      if (t.state) {
-        t._count(ed);
-      }
-     },
-    
-    getInfo: function() {
-      return {
-        longname : 'Text Metrics Plugin',
-        author : 'Scriptito, LLC',
-        authorurl : 'http://www.scriptito.com',
-        infourl : 'http://www.scriptito.com',
-        version : "1.2.2"
-      };
-    }
-    
-  });
-
-  tinymce.PluginManager.add('s_textmetrics', scriptito.plugins.TextMetrics);
+    tinymce.PluginManager.add('s_textmetrics', scriptito.plugins.TextMetrics);
 
 })();
--- plugins/s_textmetrics/text_utils.js
+++ plugins/s_textmetrics/text_utils.js
@@ -1,19 +1,3 @@
-/**
- * Copyright 2010, Scriptito LLC.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 sTextUtils={STATE_WHITESPACE:0,STATE_TAG:1,STATE_WORD:2,STATE_CHAR_REF:3,CHAR_CODE_AMP:"&".charCodeAt(0),CHAR_CODE_CARRIAGE:"\r".charCodeAt(0),CHAR_CODE_EXCLAIM:"!".charCodeAt(0),CHAR_CODE_GT:">".charCodeAt(0),CHAR_CODE_LT:"<".charCodeAt(0),CHAR_CODE_NEWLINE:"\n".charCodeAt(0),CHAR_CODE_PERIOD:".".charCodeAt(0),CHAR_CODE_QUESTION:"?".charCodeAt(0),CHAR_CODE_SEMI:";".charCodeAt(0),CHAR_CODE_SPACE:" ".charCodeAt(0),getMetricsForHtml:function(g,p){var r=this.STATE_WHITESPACE;var d=this.STATE_WHITESPACE;
 var j=0;var h=0;var q=0;var f=0;var k=0;var o=0;var n=0;var b=0;var m=0;var l=0;var a="";p=p||80;for(var e=0;e<g.length;e++){var c=g.charCodeAt(e);switch(r){case this.STATE_WHITESPACE:if(c==this.CHAR_CODE_LT){m=e;d=r;r=this.STATE_TAG}else{j++;k++;if(k>=p){f++;k=0}if(c==this.CHAR_CODE_AMP){m=e;d=r;r=this.STATE_CHAR_REF}else{if(this.isPunctuation(c)){}else{if(!this.isWhitespace(c)){q++;b++;if(b==1){o++}r=this.STATE_WORD}else{h++}}}}break;case this.STATE_TAG:if(c==this.CHAR_CODE_GT){var a=g.substring(m+1,e).toLowerCase();
 if(a=="p"||a=="li"){n++;f++;k=0;b=0;r=this.STATE_WHITESPACE}else{r=d}}break;case this.STATE_CHAR_REF:if(c==this.CHAR_CODE_SEMI){var a=g.substring(m+1,e).toLowerCase();if(a=="nbsp"){h++;r=this.STATE_WHITESPACE}else{if(d==this.STATE_WHITESPACE){q++}r=this.STATE_WORD}}break;case this.STATE_WORD:if(c==this.CHAR_CODE_LT){m=e;d=r;r=this.STATE_TAG}else{j++;k++;if(k>=p){f++;k=0}b++;if(c==this.CHAR_CODE_AMP){m=e;d=r;r=this.STATE_CHAR_REF}else{if(this.isPunctuation(c)){b=0;r=this.STATE_WHITESPACE}else{if(this.isWhitespace(c)){h++;
